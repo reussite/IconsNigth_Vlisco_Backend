d71163d05d7df76b7efd9f917ece999ced2b5e64
pascal <pascal.dlm@yahoo.com>
2026-07-06 18:15:04 +0200

    change backend port

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Serveur démarré sur le port ${PORT}`);
+  console.log(`Accessible sur le réseau via http://<TON_IP>:${PORT}`);
 });

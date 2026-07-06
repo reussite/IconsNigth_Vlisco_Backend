ae149fb832b09e207800473174455894288bf381
pascal <pascal.dlm@yahoo.com>
2026-07-06 16:34:48 +0200

    add mennelick  contact in seeder

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,13 +2,15 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 const guests = [
-  { firstName: 'Saoud', lastName: 'LANDIOUN', whatsappNumber: '+97484124' },
+  { firstName: 'Saoud', lastName: 'LANDIOUN', whatsappNumber: '+22997484124' },
   { firstName: 'Rizkiath', lastName: 'YAYA NADJO', whatsappNumber: '+22995356886' },
   { firstName: 'Célia', lastName: 'AYIVI', whatsappNumber: '+22996121994' },
   { firstName: 'Elom', lastName: 'Tsaklidji', whatsappNumber: '+22997679061' },
   { firstName: 'Ulrich', lastName: 'NAGO', whatsappNumber: '+22997138452' },
+  { firstName: 'Mennelick', lastName: 'ADJOU', whatsappNumber: '++22997950086' },
   // remplace/complète par tes vrais invités de test
 ];
+
 
 async function main() {
   for (const guest of guests) {
